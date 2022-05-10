b8263e7bb34562a36b29ac2e21fcc09e250bb399
dasmikko <dasmikko@gmail.com>
2022-05-10 07:28:50 +0200

    -

--- keymaps/rekna/rules.mk
+++ keymaps/rekna/rules.mk
@@ -1,4 +1,15 @@
+BOOTLOADER = caterina
+
 OLED_ENABLE = yes
+WPM_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+LTO_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no 
+MAGIC_ENABLE = no
+MUSIC_ENABLE = no
