28b5cc63f2f7c5fbfc135c7bd08f802cee32767c
dasmikko <dasmikko@gmail.com>
2022-09-08 21:23:40 +0200

    tap dance stuff and clean up config

--- keymaps/rekna/keymap.c
+++ keymaps/rekna/keymap.c
@@ -123,10 +123,10 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY] = LAYOUT(
-        KC_TAB  ,  KC_Q          , KC_W    ,  KC_E   ,  KC_R          ,  KC_T          ,                                                                                   KC_Y     ,  KC_U     ,  KC_I     ,  TD(TD_O_OE)  ,  KC_P     ,  KC_BSPC, 
-        KC_LSFT ,  TD(TD_A_AA)   , KC_S    ,  KC_D          ,  KC_F          ,  KC_G          ,                                                                                   KC_H     ,  KC_J     ,  KC_K     ,  KC_L         ,  KC_SCLN  ,  KC_ENT, 
-        KC_LCTL ,  KC_Z  , KC_X  , KC_C    ,  KC_V          ,  KC_B          ,  KC_LEFT       ,  KC_RGHT   ,                         KC_UP  ,  KC_DOWN               ,  KC_N   ,  KC_M     ,  KC_COMM  ,  KC_DOT   ,  KC_SLSH      ,  KC_RCTL, 
-                                   KC_LGUI ,  KC_LALT       ,  MO(_SYM_ALT)  ,  KC_SPC        ,  NAV       ,                         SYM    ,  MT(MOD_RALT, KC_ESC)  ,  FKEYS  ,  TGAMING  ,  TFKEYS
+        KC_TAB  ,  KC_Q          , KC_W    ,  KC_E     ,  KC_R          ,  KC_T     ,                                                                                   KC_Y     ,  KC_U     ,  KC_I     ,  TD(TD_O_OE)  ,  KC_P     ,  KC_BSPC, 
+        KC_LSFT ,  TD(TD_A_AA)   , KC_S    ,  KC_D     ,  KC_F          ,  KC_G     ,                                                                                   KC_H     ,  KC_J     ,  KC_K     ,  KC_L         ,  KC_SCLN  ,  KC_ENT, 
+        KC_LCTL ,  KC_Z  , KC_X  , KC_C    ,  KC_V     ,  KC_B          ,  KC_LEFT  ,  KC_RGHT   ,                         KC_UP  ,  KC_DOWN               ,  KC_N   ,  KC_M     ,  KC_COMM  ,  KC_DOT   ,  KC_SLSH      ,  KC_RCTL, 
+                                   KC_LGUI ,  KC_LALT  ,  MO(_SYM_ALT)  ,  KC_SPC   ,  NAV       ,                         SYM    ,  MT(MOD_RALT, KC_ESC)  ,  FKEYS  ,  TGAMING  ,  TFKEYS
     ),
     [_GAMING] = LAYOUT(
         KC_GRV  ,  KC_1     , KC_2  ,  KC_3  ,  KC_4  ,  KC_5  ,                                                           KC_6  ,  KC_7     ,  KC_8     ,  KC_9     ,  KC_0   ,  KC_BSPC, 
