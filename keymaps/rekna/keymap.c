fffbae0bfdd3c25b5e2bbf44686a86b4b1ca0916
Mikkel Holm Jensen <mik@danak.dk>
2022-09-08 09:22:00 +0200

    clean up keymap file

--- keymaps/rekna/keymap.c
+++ keymaps/rekna/keymap.c
@@ -26,6 +26,8 @@ enum layers {
 #define NAV      MO(_NAV)
 #define FKEYS    MO(_FUNCTION)
 #define ADJUST   MO(_ADJUST)
+#define TGAMING  TG(_GAMING)
+#define TFKEYS   TG(_FUNCTION)
 
 #define CTL_ESC  MT(MOD_LCTL, KC_ESC)
 #define CTL_QUOT MT(MOD_RCTL, KC_QUOTE)
@@ -67,10 +69,10 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY] = LAYOUT(
-    KC_TAB  , KC_Q  , KC_W  , KC_E    ,  KC_R     ,  KC_T          ,                                                                       KC_Y          ,  KC_U        ,  KC_I          ,  KC_O   ,  KC_P     ,  KC_BSPC, 
-    KC_LSFT , KC_A  , KC_S  , KC_D    ,  KC_F     ,  KC_G          ,                                                                       KC_H          ,  KC_J        ,  KC_K          ,  KC_L   ,  KC_SCLN  ,  KC_ENT, 
-    KC_LCTL , KC_Z  , KC_X  , KC_C    ,  KC_V     ,  KC_B          ,  KC_LEFT , KC_RGHT   ,            KC_UP    ,  KC_DOWN               , KC_N          ,  KC_M        ,  KC_COMM       ,  KC_DOT ,  KC_SLSH  ,  KC_RCTL, 
-                              KC_LGUI ,  KC_LALT  ,  MO(_SYM_ALT)  ,  KC_SPC  , MO(_NAV)  ,            MO(_SYM) ,  MT(MOD_RALT, KC_ESC)  , MO(_FUNCTION) ,  TG(_GAMING) ,  TG(_FUNCTION)
+    KC_TAB  , KC_Q  , KC_W  , KC_E    ,  KC_R     ,  KC_T          ,                                                                     KC_Y   ,  KC_U     ,  KC_I     ,  KC_O   ,  KC_P     ,  KC_BSPC, 
+    KC_LSFT , KC_A  , KC_S  , KC_D    ,  KC_F     ,  KC_G          ,                                                                     KC_H   ,  KC_J     ,  KC_K     ,  KC_L   ,  KC_SCLN  ,  KC_ENT, 
+    KC_LCTL , KC_Z  , KC_X  , KC_C    ,  KC_V     ,  KC_B          ,  KC_LEFT , KC_RGHT   ,            KC_UP  ,  KC_DOWN               , KC_N   ,  KC_M     ,  KC_COMM  ,  KC_DOT ,  KC_SLSH  ,  KC_RCTL, 
+                              KC_LGUI ,  KC_LALT  ,  MO(_SYM_ALT)  ,  KC_SPC  , NAV       ,            SYM    ,  MT(MOD_RALT, KC_ESC)  , FKEYS  ,  TGAMING  ,  TFKEYS
   ),
 	[_GAMING] = LAYOUT(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, KC_TAB, KC_LSFT, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_UP, KC_I, KC_P, KC_ENT, KC_NO, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_ESC, KC_Z, KC_X, KC_C, KC_V, KC_SPC, KC_B, KC_N, KC_M, KC_TRNS, KC_NO),
 	[_COLEMAK_DH] = LAYOUT(KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_BSPC, MT(MOD_LCTL, KC_ESC), KC_A, KC_R, KC_S, KC_T, KC_G, KC_M, KC_N, KC_E, KC_I, KC_O, MT(MOD_RCTL, KC_QUOTE), KC_LSFT, KC_Z, KC_X, KC_C, KC_D, KC_V, KC_LBRC, KC_CAPS, MO(5), KC_RBRC, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_LGUI, MO(6), KC_LALT, KC_SPC, MO(3), MO(4), KC_SPC, KC_TRNS, KC_RGUI, KC_APP),
