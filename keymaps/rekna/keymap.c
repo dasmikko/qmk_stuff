a018fa143d5ce0edeb76694dc62e9bbff07e4698
dasmikko <dasmikko@gmail.com>
2022-09-10 14:59:26 +0200

    switch around agaaaaaaaain

--- keymaps/rekna/keymap.c
+++ keymaps/rekna/keymap.c
@@ -44,10 +44,10 @@ enum layers {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY] = LAYOUT(
-        KC_TAB  ,  KC_Q  ,  KC_W  ,  KC_E     ,  KC_R          ,  KC_T          ,                                                                                               KC_Y     ,  KC_U     ,  KC_I     ,  KC_O    ,  KC_P     ,  KC_LBRC, 
-        KC_LSFT ,  KC_A  ,  KC_S  ,  KC_D     ,  KC_F          ,  KC_G          ,                                                                                               KC_H     ,  KC_J     ,  KC_K     ,  KC_L    ,  KC_SCLN  ,  KC_QUOT, 
-        KC_LCTL ,  KC_Z  ,  KC_X  ,  KC_C     ,  KC_V          ,  KC_B          ,  KC_LEFT   ,  KC_RGHT   ,                                 MT(MOD_RALT, KC_ESC)  ,  KC_UP   ,  KC_N     ,  KC_M     ,  KC_COMM  ,  KC_DOT  ,  KC_SLSH  ,  MT(MOD_RCTL, KC_ENT), 
-                                     KC_LGUI  ,  KC_LALT       ,  MO(_SYM_ALT)  ,  KC_SPC    ,  NAV       ,                                 SYM                   ,  KC_DOWN ,  KC_BSPC  ,  TGAMING  ,  TFKEYS
+        KC_TAB  ,  KC_Q  ,  KC_W  ,  KC_E     ,  KC_R          ,  KC_T          ,                                                                                               KC_Y     ,  KC_U     ,  KC_I     ,  KC_O    ,  KC_P     ,  KC_BSPC, 
+        KC_LSFT ,  KC_A  ,  KC_S  ,  KC_D     ,  KC_F          ,  KC_G          ,                                                                                               KC_H     ,  KC_J     ,  KC_K     ,  KC_L    ,  KC_SCLN  ,  KC_ENT, 
+        KC_LCTL ,  KC_Z  ,  KC_X  ,  KC_C     ,  KC_V          ,  KC_B          ,  KC_LEFT   ,  KC_RGHT   ,                                 MT(MOD_RALT, KC_ESC)  ,  KC_UP   ,  KC_N     ,  KC_M     ,  KC_COMM  ,  KC_DOT  ,  KC_SLSH  ,  MT(MOD_RSFT, KC_RCTL), 
+                                     KC_LGUI  ,  KC_LALT       ,  MO(_SYM_ALT)  ,  KC_SPC    ,  NAV       ,                                 SYM                   ,  KC_DOWN ,  KC_ESC  ,  TGAMING  ,  TFKEYS
     ),
     [_GAMING] = LAYOUT(
         KC_GRV  ,  KC_1     , KC_2  ,  KC_3  ,  KC_4  ,  KC_5  ,                                                           KC_6  ,  KC_7     ,  KC_8     ,  KC_9     ,  KC_0   ,  KC_BSPC, 
@@ -62,8 +62,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                      KC_LGUI  ,  KC_LALT       ,  MO(_SYM_ALT)  ,  KC_SPC    ,  NAV       ,                                 SYM    ,  MT(MOD_RALT, KC_ESC)  ,  FKEYS    ,  TGAMING  ,  TFKEYS
     ),
     [_NAV] = LAYOUT(
-        KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  , KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,                                                              KC_INS   ,  KC_HOME  ,  KC_PGUP  ,  KC_PAUS  ,  KC_TRNS  ,  KC_LBRC, 
-        KC_TRNS  ,  KC_LGUI  ,  KC_LALT  , KC_LCTL  ,  KC_LSFT  ,  KC_TRNS  ,                                                              KC_DEL   ,  KC_END   ,  KC_PGDN  ,  KC_SCRL  ,  KC_SCLN  ,  KC_QUOT, 
+        KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  , KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,                                                              KC_INS   ,  KC_HOME  ,  KC_PGUP  ,  KC_PAUS  ,  KC_LBRC  ,  KC_TRNS, 
+        KC_TRNS  ,  KC_LGUI  ,  KC_LALT  , KC_LCTL  ,  KC_LSFT  ,  KC_TRNS  ,                                                              KC_DEL   ,  KC_END   ,  KC_PGDN  ,  KC_SCLN  ,  KC_QUOT  ,  KC_TRNS, 
         KC_TRNS  ,  KC_TRNS  ,  KC_NUM   , KC_CAPS  ,  KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,  KC_SCRL  ,              KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,  KC_MPRV  ,  KC_MPLY  ,  KC_MNXT  ,  KC_TRNS  ,  KC_PSCR,
                                            KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,              KC_TRNS  ,  KC_TRNS  ,  KC_MUTE  ,  KC_VOLD  ,  KC_VOLU
     ),
