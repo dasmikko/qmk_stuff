765894f703c5543524554f6882158a1d5ca57246
Mikkel Holm Jensen <mik@danak.dk>
2022-09-13 09:11:20 +0200

    swap v and space in the gaming layout to make hand more relaxed

--- keymaps/rekna/keymap.c
+++ keymaps/rekna/keymap.c
@@ -62,15 +62,15 @@ combo_t key_combos[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY] = LAYOUT(
         KC_TAB  ,  KC_Q  ,  KC_W  ,  KC_E     ,  KC_R     ,  KC_T          ,                                                                                               KC_Y     ,  KC_U     ,  KC_I     ,  KC_O    ,  KC_P     ,  KC_BSPC, 
-        KC_LSFT ,  KC_A  ,  KC_S  ,  KC_D     ,  KC_F     ,  KC_G          ,                                                                                               KC_H     ,  KC_J     ,  KC_K     ,  KC_L    ,  KC_NUHS  ,  KC_ENT, 
+        KC_LSFT ,  KC_A  ,  KC_S  ,  KC_D     ,  KC_F     ,  KC_G          ,                                                                                               KC_H     ,  KC_J     ,  KC_K     ,  KC_L    ,  KC_NUHS   ,  KC_ENT, 
         KC_LCTL ,  KC_Z  ,  KC_X  ,  KC_C     ,  KC_V     ,  KC_B          ,  KC_LEFT   ,  KC_RGHT   ,                                 MT(MOD_RALT, KC_ESC)  ,  KC_UP   ,  KC_N     ,  KC_M     ,  KC_COMM  ,  KC_DOT  ,  KC_SLSH  ,  MT(MOD_RSFT, KC_RCTL), 
                                      KC_LGUI  ,  KC_LALT  ,  MO(_SYM_ALT)  ,  KC_SPC    ,  NAV       ,                                 SYM                   ,  KC_DOWN ,  KC_ESC  ,  TGAMING  ,  TFKEYS
     ),
     [_GAMING] = LAYOUT(
         KC_GRV  ,  KC_1     , KC_2  ,  KC_3  ,  KC_4  ,  KC_5  ,                                                           KC_6  ,  KC_7     ,  KC_8     ,  KC_9     ,  KC_0   ,  KC_BSPC, 
         KC_TAB  ,  KC_LSFT  , KC_Q  ,  KC_W  ,  KC_E  ,  KC_R  ,                                                           KC_T  ,  KC_Y     ,  KC_UP    ,  KC_I     ,  KC_P   ,  KC_ENT, 
-        KC_NO   ,  KC_LCTL  , KC_A  ,  KC_S  ,  KC_D  ,  KC_F  ,  KC_G  ,  KC_H    ,                      KC_J  ,  KC_K  , KC_L  ,  KC_LEFT  ,  KC_DOWN  ,  KC_RGHT  ,  KC_NO  ,  KC_ESC, 
-                                       KC_Z  ,  KC_X  ,  KC_C  ,  KC_V  ,  KC_SPC  ,                      KC_B  ,  KC_N  , KC_M  ,  KC_TRNS  ,  KC_NO
+        KC_NO   ,  KC_LCTL  , KC_A  ,  KC_S  ,  KC_D  ,  KC_F  ,  KC_G    ,  KC_H  ,                      KC_J  ,  KC_K  , KC_L  ,  KC_LEFT  ,  KC_DOWN  ,  KC_RGHT  ,  KC_NO  ,  KC_ESC, 
+                                       KC_Z  ,  KC_X  ,  KC_C  ,  KC_SPC  ,  KC_V  ,                      KC_B  ,  KC_N  , KC_M  ,  KC_TRNS  ,  KC_NO
     ),
     [_ALT] = LAYOUT(
         LALT(KC_TAB)  ,  KC_F9  ,  KC_F10  ,  KC_F11   ,  KC_F12   ,  KC_TRNS  ,                                                                            KC_P7  ,  KC_P8    ,  KC_P9    ,  KC_PSLS  ,  KC_PAST  ,  KC_PMNS  ,  
