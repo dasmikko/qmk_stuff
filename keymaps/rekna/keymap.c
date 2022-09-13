03b49dd43c95affb5e15143e8aafe605eb3631df
Mikkel Holm Jensen <mik@danak.dk>
2022-09-13 08:14:03 +0200

    remove æ from main layer, to force correct habits

--- keymaps/rekna/keymap.c
+++ keymaps/rekna/keymap.c
@@ -62,7 +62,7 @@ combo_t key_combos[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_QWERTY] = LAYOUT(
         KC_TAB  ,  KC_Q  ,  KC_W  ,  KC_E     ,  KC_R     ,  KC_T          ,                                                                                               KC_Y     ,  KC_U     ,  KC_I     ,  KC_O    ,  KC_P     ,  KC_BSPC, 
-        KC_LSFT ,  KC_A  ,  KC_S  ,  KC_D     ,  KC_F     ,  KC_G          ,                                                                                               KC_H     ,  KC_J     ,  KC_K     ,  KC_L    ,  KC_SCLN  ,  KC_ENT, 
+        KC_LSFT ,  KC_A  ,  KC_S  ,  KC_D     ,  KC_F     ,  KC_G          ,                                                                                               KC_H     ,  KC_J     ,  KC_K     ,  KC_L    ,  KC_NUHS  ,  KC_ENT, 
         KC_LCTL ,  KC_Z  ,  KC_X  ,  KC_C     ,  KC_V     ,  KC_B          ,  KC_LEFT   ,  KC_RGHT   ,                                 MT(MOD_RALT, KC_ESC)  ,  KC_UP   ,  KC_N     ,  KC_M     ,  KC_COMM  ,  KC_DOT  ,  KC_SLSH  ,  MT(MOD_RSFT, KC_RCTL), 
                                      KC_LGUI  ,  KC_LALT  ,  MO(_SYM_ALT)  ,  KC_SPC    ,  NAV       ,                                 SYM                   ,  KC_DOWN ,  KC_ESC  ,  TGAMING  ,  TFKEYS
     ),
