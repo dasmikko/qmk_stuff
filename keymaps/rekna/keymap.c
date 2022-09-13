bfaf5f23eed7859896c4ee33f05689d05d3ff827
Mikkel Holm Jensen <mik@danak.dk>
2022-09-13 12:25:18 +0200

    add more home/end/pgup/pgdown

--- keymaps/rekna/keymap.c
+++ keymaps/rekna/keymap.c
@@ -107,8 +107,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NAV] = LAYOUT(
         KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  , KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,                                                              KC_INS   ,  KC_HOME  ,  KC_PGUP  ,  KC_PAUS  ,  KC_LBRC  ,  KC_TRNS, 
         KC_TRNS  ,  KC_LGUI  ,  KC_LALT  , KC_LCTL  ,  KC_LSFT  ,  KC_TRNS  ,                                                              KC_DEL   ,  KC_END   ,  KC_PGDN  ,  KC_SCLN  ,  KC_QUOT  ,  KC_TRNS, 
-        KC_TRNS  ,  KC_TRNS  ,  KC_NUM   , KC_CAPS  ,  KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,  KC_SCRL  ,              KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,  KC_MPRV  ,  KC_MPLY  ,  KC_MNXT  ,  KC_TRNS  ,  KC_PSCR,
-                                           KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,              KC_TRNS  ,  KC_TRNS  ,  KC_MUTE  ,  KC_VOLD  ,  KC_VOLU
+        KC_TRNS  ,  KC_TRNS  ,  KC_NUM   , KC_CAPS  ,  KC_TRNS  ,  KC_TRNS  ,  KC_HOME  ,  KC_END   ,              KC_TRNS  ,  KC_PGUP  ,  KC_TRNS  ,  KC_MPRV  ,  KC_MPLY  ,  KC_MNXT  ,  KC_TRNS  ,  KC_PSCR,
+                                           KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,              KC_TRNS  ,  KC_PGDN  ,  KC_MUTE  ,  KC_VOLD  ,  KC_VOLU
     ),
     [_SYM] = LAYOUT(
         KC_GRV   ,  KC_1        ,  KC_2        ,  KC_3        ,  KC_4        ,  KC_5,                                                                                                   KC_6        ,  KC_7        ,  KC_8        ,  KC_9        ,  KC_0     ,  KC_MINS, 
