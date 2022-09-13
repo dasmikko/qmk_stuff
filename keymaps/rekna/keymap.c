606430e2855b5afbbcb3f7d70cd119f46a593e2d
Mikkel Holm Jensen <mik@danak.dk>
2022-09-13 14:29:09 +0200

    numpad stuff

--- keymaps/rekna/keymap.c
+++ keymaps/rekna/keymap.c
@@ -99,10 +99,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                        KC_Z  ,  KC_X  ,  KC_C  ,  KC_SPC  ,  KC_V  ,                      KC_B  ,  KC_N  , KC_M  ,  KC_TRNS  ,  KC_NO
     ),
     [_ALT] = LAYOUT(
-        LALT(KC_TAB)  ,  KC_F9  ,  KC_F10  ,  KC_F11   ,  KC_F12   ,  KC_TRNS  ,                                                                            KC_P7  ,  KC_P8    ,  KC_P9    ,  KC_PSLS  ,  KC_PAST  ,  KC_PMNS  ,  
-        KC_TRNS       ,  KC_F5  ,  KC_F6   ,  KC_F7    ,  KC_F8    ,  KC_TRNS  ,                                                                            KC_P4  ,  KC_P5    ,  KC_P6    ,  KC_PPLS  ,  KC_PCMM  ,  KC_PEQL  ,  
-        KC_TRNS       ,  KC_F1  ,  KC_F2   ,  KC_F3    ,  KC_F4    ,  KC_TRNS  ,  KC_TRNS  ,  KC_LALT  ,                            KC_LSFT  ,  KC_TRNS  ,  KC_P1  ,  KC_P2    ,  KC_P3    ,  KC_PENT  ,  KC_TRNS  ,  KC_RCTL  , 
-                                              KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,                            KC_LCTL  ,  KC_TRNS  ,  KC_P0  ,  KC_TRNS  ,  KC_KP_DOT
+        LALT(KC_TAB)  ,  KC_F9  ,  KC_F10  ,  KC_F11   ,  KC_F12   ,  KC_TRNS  ,                                                                            KC_P7  ,  KC_P8      ,  KC_P9    ,  KC_PSLS  ,  KC_PAST  ,  KC_PMNS  ,  
+        KC_TRNS       ,  KC_F5  ,  KC_F6   ,  KC_F7    ,  KC_F8    ,  KC_TRNS  ,                                                                            KC_P4  ,  KC_P5      ,  KC_P6    ,  KC_PPLS  ,  KC_PCMM  ,  KC_PEQL  ,  
+        KC_TRNS       ,  KC_F1  ,  KC_F2   ,  KC_F3    ,  KC_F4    ,  KC_TRNS  ,  KC_TRNS  ,  KC_LALT  ,                            KC_LSFT  ,  KC_TRNS  ,  KC_P1  ,  KC_P2      ,  KC_P3    ,  KC_PENT  ,  KC_TRNS  ,  KC_RCTL  , 
+                                              KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  ,                            KC_LCTL  ,  KC_TRNS  ,  KC_P0  ,  KC_KP_DOT  ,  KC_KP_DOT
     ),
     [_NAV] = LAYOUT(
         KC_TRNS  ,  KC_TRNS  ,  KC_TRNS  , KC_TRNS  ,  KC_TRNS        ,  LSFT(KC_PGUP)  ,                                                              KC_INS   ,  KC_HOME  ,  KC_PGUP  ,  KC_PAUS  ,  KC_LBRC  ,  KC_TRNS, 
